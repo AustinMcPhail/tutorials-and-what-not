b12a5400897070b399e5ff33bec62ba096e5e5e1
Austin McPhail <austin@mcphail.dev>
2020-02-07 08:03:03 -0600

    chore(auth.js): adding imports to auth.js

--- react-front-to-back/contact-keeper/routes/auth.js
+++ react-front-to-back/contact-keeper/routes/auth.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+const config = require('config');
+const User = require('../models/User');
 
 const router = express.Router();
 
